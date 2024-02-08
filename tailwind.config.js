707cab8e1084032440129e539a091ea6037645cc
DevEmmy <eolaosebikan60@gmail.com>
2024-02-08 04:58:01 +0100

    basic setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        c1: "#104D6F",
+        c2: "#E69C24",
+        c3: "#FFECCC",
+        c4: "#8B7357",
+        sh1: "#3C6E93",
+        sh2: "#6292B8",
+        sh3: "#88B6DE",
+        sh4: "#AEDDFF"
+      },
+      fontSize: {
+        sm: "14px",
+        md: "20px",
+        mdl: "32px",
+        lg: "44px",
+        xl: "56px"
+      }
     },
   },
   plugins: [],
